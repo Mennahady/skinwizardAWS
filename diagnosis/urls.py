5f38b3f8b234ad7ab6278a320c59c31aacdd2405
Menna Abdelhady <102001815+Mennahady@users.noreply.github.com>
2025-06-23 03:48:08 +0300

    Update urls.py

--- diagnosis/urls.py
+++ diagnosis/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PatientViewSet, SkinImageViewSet, DiagnosisViewSet
 
 router = DefaultRouter()
-router.register(r'patients', PatientViewSet)
+#router.register(r'patients', PatientViewSet)
 router.register(r'images', SkinImageViewSet, basename='skin-image')
 router.register(r'diagnoses', DiagnosisViewSet, basename='diagnosis')
 
